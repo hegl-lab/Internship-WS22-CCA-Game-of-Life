c0ad43112e74e1022622aeb1527bd6ae8e859ab3
Adrian Becker <git@adrian-becker.de>
2022-12-03 14:00:57 +0100

    Add support for arguments of the type std::string

--- src/glfw-abstraction/Arguments.h
+++ src/glfw-abstraction/Arguments.h
@@ -19,6 +19,11 @@ std::string generate_arguments(T arg) {
     return "#define " + arg.name + " " + std::to_string(arg.value) + "\n";
 }
 
+template<typename U=std::string>
+std::string generate_arguments(Argument<std::string> arg) {
+    return "#define " + arg.name + " " + arg.value + "\n";
+}
+
 template<typename... Arguments>
 std::string generate_arguments_with_default_marker(Arguments... arguments) {
     return "#define default_consts\n" + generate_arguments(arguments...);
